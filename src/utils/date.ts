eeea73afba837ec167fb903d6dc6dc015b326640
enteresanlikk <zeki1198@gmail.com>
2024-12-03 00:16:04 +0300

    Fix format date.

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,7 +3,7 @@ import { padZero } from "./number";
 export const formatDate = (date: string) => {
     const dateObj = new Date(date);
 
-    const dateStr = `${dateObj.getDate()}-${padZero(dateObj.getMonth() + 1)}-${padZero(dateObj.getFullYear())}`;
+    const dateStr = `${padZero(dateObj.getDate())}-${padZero(dateObj.getMonth() + 1)}-${padZero(dateObj.getFullYear())}`;
 
     const timeStr = `${padZero(dateObj.getHours())}:${padZero(dateObj.getMinutes())}`;
 
