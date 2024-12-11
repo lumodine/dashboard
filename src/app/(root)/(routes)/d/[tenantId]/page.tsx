718311fe62d0bc579a32c8d1ff52bd5e6637b6aa
enteresanlikk <zeki1198@gmail.com>
2024-12-11 23:35:32 +0300

    Remove menu button in tenant hero.

--- src/app/(root)/(routes)/d/[tenantId]/page.tsx
+++ src/app/(root)/(routes)/d/[tenantId]/page.tsx
@@ -1,9 +1,8 @@
-import {Building2, ExternalLink} from "lucide-react";
+import {Building2} from "lucide-react";
 import Link from "next/link";
 import {AppBreadcrumb} from "@/components/common/breadcrumb";
 import {Hero} from "@/components/common/hero";
 import {TenantMenuList} from "@/components/tenant/tenant-menu-list";
-import {Button} from "@/components/ui/button";
 import tenantService from "@/services/tenant.service";
 import {TenantIframe} from "@/components/tenant/tenant-iframe";
 import {TenantIframeGroup} from "@/components/tenant/tenant-iframe-group";
@@ -21,16 +20,6 @@ export default async function TenantPage({params}: TenantPageProps) {
   return (
     <>
       <Hero
-        button={
-          <Link
-            href={process.env.NEXT_PUBLIC_QR_MENU_URL!.replace("{alias}", tenant.alias)}
-            target="_blank"
-          >
-            <Button size={"sm"} variant={"secondary"}>
-              <ExternalLink /> Menüyü gör
-            </Button>
-          </Link>
-        }
         supTitle={<Link href={"/d"}>{process.env.NEXT_PUBLIC_APP_NAME!}</Link>}
         title={tenant.name}
       />
