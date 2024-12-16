716c7141f9e81674fcb521d5665a0a979ca60a48
enteresanlikk <zeki1198@gmail.com>
2024-12-16 21:32:47 +0300

    Update header logo.

--- src/app/(auth)/components/header.tsx
+++ src/app/(auth)/components/header.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 export const Header = () => {
   return (
     <header>
-      <Link href={process.env.NEXT_PUBLIC_LANDING_URL!} target="_blank">
+      <Link className="flex gap-2 items-center justify-center" href={process.env.NEXT_PUBLIC_LANDING_URL!} target="_blank">
         <Image
           alt={`${process.env.NEXT_PUBLIC_APP_NAME} logo`}
-          className="rounded-full"
-          height={80}
+          height={50}
           loading="lazy"
-          src="https://cdn.lumodine.com/public/logo.jpg"
-          width={80}
+          src="https://cdn.lumodine.com/public/logo.jpg?v=1"
+          width={50}
         />
+        <b>{process.env.NEXT_PUBLIC_APP_NAME}</b>
       </Link>
     </header>
   );
--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -22,10 +22,10 @@ export const Header = () => {
           <Link className="flex gap-2 items-center justify-center" href={"/d"}>
             <Image
               alt={`${process.env.NEXT_PUBLIC_APP_NAME} logo`}
-              className="rounded-full h-full"
+              className="h-full"
               height={50}
               loading="lazy"
-              src="https://cdn.lumodine.com/public/logo.jpg"
+              src="https://cdn.lumodine.com/public/logo.jpg?v=1"
               width={50}
             />
             <b>{process.env.NEXT_PUBLIC_APP_NAME}</b>
