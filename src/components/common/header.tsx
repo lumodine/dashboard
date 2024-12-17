e46a572b1fc433fb5b0bcb22800e20af28e1a94b
enteresanlikk <zeki1198@gmail.com>
2024-12-17 22:29:24 +0300

    Add mobile menu.

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -3,9 +3,10 @@
 import {redirect} from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
-import {LogOut, User} from "lucide-react";
+import {LogOut, Menu, User} from "lucide-react";
 import {Button} from "@/components/ui/button";
 import authService from "@/services/auth.service";
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
 
 export const Header = () => {
   const handleLogout = async () => {
@@ -28,10 +29,10 @@ export const Header = () => {
               src="https://cdn.lumodine.com/public/logo.jpg?v=1"
               width={50}
             />
-            <b>{process.env.NEXT_PUBLIC_APP_NAME}</b>
+            <b className="hidden sm:block">{process.env.NEXT_PUBLIC_APP_NAME}</b>
           </Link>
         </div>
-        <div className="flex gap-2 items-center justify-center">
+        <div className="hidden md:flex gap-2 items-center justify-center">
           <Link href={"/d/my-account"}>
             <Button variant={"ghost"}>
               <User /> My account
@@ -40,6 +41,26 @@ export const Header = () => {
           <Button variant={"destructive"} onClick={handleLogout}>
             Log out <LogOut />
           </Button>
+        </div>
+
+        <div className="md:hidden">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button className="p-2" variant="ghost">
+                <Menu className="h-6 w-6" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent align="end" className="w-48 flex flex-col gap-2">
+              <Link className="w-full" href={"/d/my-account"}>
+                <Button className="w-full" variant={"default"}>
+                  <User /> My account
+                </Button>
+              </Link>
+              <Button variant={"destructive"} onClick={handleLogout}>
+                Log out <LogOut />
+              </Button>
+            </PopoverContent>
+          </Popover>
         </div>
       </nav>
     </header>
