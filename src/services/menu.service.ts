6e8b4d728e11693b4371dc7082e29a79ca353724
enteresanlikk <zeki1198@gmail.com>
2024-12-28 00:46:24 +0300

    Update create menu service url.

--- src/services/menu.service.ts
+++ src/services/menu.service.ts
@@ -1,7 +1,7 @@
 import axios from "@/lib/axios";
 
 const create = async (tenantId: string, items: any[]) => {
-  const {data} = await axios.post(`/tenants/${tenantId}/items/menu`, {
+  const {data} = await axios.post(`/tenants/${tenantId}/menu`, {
     items,
   });
 
