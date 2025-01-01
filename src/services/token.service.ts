f8db3809b8f6bd603c12ac4d76c91c1f4b6a7384
enteresanlikk <zeki1198@gmail.com>
2025-01-01 23:21:47 +0300

    Update token cookie expire date.

--- src/services/token.service.ts
+++ src/services/token.service.ts
@@ -4,7 +4,15 @@ const COOKIE_KEY = "token";
 
 const getToken = async () => await get(COOKIE_KEY);
 
-const setToken = async (token: string) => await set(COOKIE_KEY, token);
+const setToken = async (token: string) => {
+  const cookieExpire = new Date();
+
+  cookieExpire.setHours(cookieExpire.getHours() + 3);
+
+  await set(COOKIE_KEY, token, {
+    expires: cookieExpire,
+  });
+};
 
 const removeToken = async () => await remove(COOKIE_KEY);
 
